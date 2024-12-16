7b17013fea9102f6decf15f28358589727d20828
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2024-12-16 17:36:00 +0530

    ui changes

--- src/components/sections/careerSections/HiringCriteria.tsx
+++ src/components/sections/careerSections/HiringCriteria.tsx
@@ -18,12 +18,12 @@ const HiringCriteria = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
-        className="h-full w-full  px-6 relative z-10 flex flex-col gap-8  md:px-14  lg:px-24 md:flex-row lg:justify-evenly xl:px-32 "
+        className=" items-center h-full w-full  px-6 relative z-10 flex flex-col gap-8  md:px-14  lg:px-24 md:flex-row lg:justify-evenly xl:px-32 "
       >
         <motion.div
           variants={planetVariants("right")}
           viewport={{ once: true }}
-          className="flex  justify-center items-center"
+          className="flex justify-center items-center"
         >
           <img
             height={400}
@@ -36,29 +36,29 @@ const HiringCriteria = () => {
 
         <motion.div
           variants={slideIn("left", "spring", 0.2, 1.75)}
-          className="flex-[0.95] flex flex-col justify-center items-center gap-2 md:gap-5 xl:gap-10"
+          className=" mt-10 flex-[0.95] flex flex-col justify-center items-center gap-2 md:gap-5 xl:gap-10"
         >
           <h3
-            className="title font-bold uppercase text-4xl text-gradient"
+            className="title font-bold uppercase text-3xl md:text-5xl text-gradient"
             style={{ textShadow: "2px -2px white" }}
           >
             we are now hiring{" "}
           </h3>
-          <h4 className="text-gradient   tracking-[.5rem] text-2xl font-medium  uppercase">
-            Hiring criteria
+          <h4 className="tracking-[.5rem] text-sm md:text-2xl font-medium  uppercase text-gray-50">
+            Our Hiring criteria
           </h4>
 
-          <div className="flex flex-col gap-4 md:gap-5 xl:gap-12 ">
+          <div className="mt-5 flex flex-col gap-4 md:gap-5 xl:gap-12 ">
             {hiringCriteria.map((feature, index) => {
               return (
-                <div className="flex justify-start items-center" key={index}>
-                  <div className="bg-[#8048ef] flex justify-center items-center w-12 h-12 rounded-xl mt-1 md:w-16 md:h-16 md:text-lg xl:w-20 xl:h-20 xl:rounded-2xl xl:text-xl">
-                    <h3 className="font-extrabold text-2xl text-gray-300">
+                <div className="flex justify-start items-start" key={index}>
+                  <div className="bg-gradient-to-b from-[#ac48ef] to-[#3d0d5e]  flex justify-center items-center w-12 h-12 rounded-xl mt-1 md:w-16 md:h-16 md:text-lg xl:w-20 xl:h-20 xl:rounded-2xl xl:text-xl">
+                    <h3 className="font-extrabold text-3xl text-gray-50">
                       0{index + 1}
                     </h3>
                   </div>
 
-                  <p className="flex-1 ml-[30px] font-normal text-sm  text-gray-300  md:ml-[36px]    xl:leading-[38px] xl:ml-[48px] xl:max-w-[370px]">
+                  <p className="font-medium md:text-lg flex-1 ml-[30px] font-normal text-sm  text-gray-50 md:ml-[36px]    xl:leading-[38px] xl:ml-[48px] xl:max-w-[370px]">
                     {feature}
                   </p>
                 </div>
--- src/components/sections/careerSections/JoinUs.tsx
+++ src/components/sections/careerSections/JoinUs.tsx
@@ -1,10 +1,9 @@
-import Button from "../../Button";
+// import Button from "../../Button";
 
 const JoinUs = () => {
   return (
-    <div className="bg-[#181424] md:min-h-screen w-full flex justify-center items-center">
-      <div className="magic-card2 flex-col z-10 max-w-[600px] m-2  !p-5 md:!p-16 gap-5">
-        <div className="absolute h-full w-full bg-black rounded-lg opacity-20 backdrop-blur-lg "></div>
+    <div className="bg-[#181424]  w-full flex  text-center justify-center items-center">
+      <div className=" flex-col z-10 w-[90%] md:w-[60%]  bg-gradient-to-b from-[#8546d3]/70 to-[#181424]  flex  justify-evenly  items-center p-8 md:p-20 rounded-lg gap-5">
         <h2
           className="text-gradient uppercase text-4xl md:text-6xl font-bold "
           style={{ textShadow: "4px -4px white" }}
@@ -16,7 +15,10 @@ const JoinUs = () => {
           quo esse cupiditate odit possimus deleniti vel velit tenetur ut
           deserunt.
         </p>
-        <Button>Join Us</Button>
+
+        <button className="mt-5 md:mt-10 gradient-button text-xl uppercase !px-6 !py-4 font-bold">
+          Join Us
+        </button>
       </div>
     </div>
   );

3fe501fa4b35d60d4cae75fe104459cc7d451844
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2024-12-19 11:27:20 +0530

    color changes

--- src/pages/admin/auth/SignIn.tsx
+++ src/pages/admin/auth/SignIn.tsx
@@ -8,8 +8,8 @@ function SignIn() {
         <div className="shape-blob one"></div>
         <div className="shape-blob two"></div>
       </div>
-      <div className="w-[350px] min-h-[500px] rounded-md bg-gradient-to-br from-[#011430] to-black opa z-10 border border-[#346e9b] p-7">
-        <h1 className=" text-3xl font-bold text-[#2b89d1]">Welcome Back!</h1>
+      <div className="w-[350px] min-h-[500px] rounded-md bg-gradient-to-bl from-[#01301e] to-[#00130c] opa z-10 border border-[#349b7c] p-7">
+        <h1 className=" text-3xl font-bold text-[#2bd1a8]">Welcome Back!</h1>
         <p className="mt-2 text-xl">Let's get you logged in.</p>
 
         <form action="submit" className="mt-10">
@@ -21,7 +21,7 @@ function SignIn() {
             type="email"
             name="email"
             id="email"
-            className="focus:border-[2px] mt-1 mb-4 p-2 bg-gradient-to-b from-[#0a1e38] to-[#061222] w-full min-h-10  rounded-md border border-[#346e9b] placeholder:text-sm placeholder:text-slate-500 text-[#4cc9f0]"
+            className="focus:border-[2px] mt-1 mb-4 p-2 bg-gradient-to-b from-[#0a3830] to-[#06221a] w-full min-h-10  rounded-md border border-[#349b7c] placeholder:text-sm placeholder:text-gray-500 text-[#26e7ad]"
           />
           <label htmlFor="password" className="text-sm">
             Password
@@ -31,15 +31,15 @@ function SignIn() {
             type="password"
             name="password"
             id="password"
-            className="focus:border-[2px] mt-1 p-2 bg-gradient-to-b from-[#0a1e38] to-[#061222] w-full min-h-10  rounded-md border border-[#346e9b] placeholder:text-sm placeholder:text-slate-500 text-[#4cc9f0]"
+            className="focus:border-[2px] mt-1 p-2 bg-gradient-to-b from-[#0a3830] to-[#06221a] w-full min-h-10  rounded-md border border-[#349b7c] placeholder:text-sm placeholder:text-gray-500 text-[#26e7ad]"
           />
           <button
             type="submit"
-            className=" mt-10 w-full bg-gradient-to-b from-[#0a61b2] to-[#001a41] hover:from-[#007bff] hover:to-[#001d6b] min-h-10 rounded-md text-center uppercase font-semibold"
+            className=" mt-10 w-full bg-gradient-to-b from-[#0ab290] to-[#004136] hover:from-[#00ffbf] hover:to-[#006b49] min-h-10 rounded-md text-center uppercase font-semibold"
           >
             Log in
           </button>
-          <p className="text-sm mt-5 text-center text-[#0096f3] hover:text-[#6fc8ff] cursor-pointer">
+          <p className="text-sm mt-5 text-center text-[#13debc] hover:text-[#6fffd6] cursor-pointer">
             forgot password?
           </p>
         </form>
